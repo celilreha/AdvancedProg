aefa2f57e696065dd5bf30ab9a70c03818fa99d5
Celil Reha <celilrehaesgice@gmail.com>
2018-12-20 02:47:47 +0300

    update

--- Project/data.js
+++ Project/data.js
@@ -5,14 +5,14 @@ window.onload = function () {
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
     var animal=["alligator","ant","bear","bee","bird","camel","cat","cheetah","chicken","chimpanzee",
         "cow","crocodile","deer","dog","dolphin","duck","eagle","elephant","fish","fly","fox","frog",
-        "giraffe","goat","goldfish","hamster","hippopotamus","horse","kangaroo","kitten","lion",
+        "giraffe","goat","goldfish","hamster","horse","kangaroo","kitten","lion",
         "lobster","monkey","octopus","owl","panda","pig","puppy","rabbit","rat","scorpion","seal",
         "shark","sheep","snail","snake","spider","squirrel","tiger","turtle","wolf","zebra"];
     var country=["Afghanistan","Argentina","Australia","Azerbaijan","Belgium","Brazil","Bulgaria",
         "Canada","China","Colombia","Croatia","Cuba","Cyprus","Czech-Republic","Denmark",,"Egypt",
         "Finland","France","Germany","Greece","India","Iran","Iraq","Ireland","Italy","Jamaica","Japan",
         "Kenya","Luxembourg","Mexico","Netherlands","Nigeria","Norway","Pakistan","Poland","Portugal","Romania",
-        "Russia","Spain","Switzerland","Syria","Turkey","Turkmenistan","United Arab Emirates","United-Kingdom","Ukraine"];
+        "Russia","Spain","Switzerland","Syria","Turkey","Turkmenistan","United-Kingdom","Ukraine"];
     var capital=["Kabul","Buenos-Aires", "Canberra","Baku","Brussels","Brasilia", "Sofia","Ottawa","Beijing","Bogota",
         "Zagreb", "Havana","Nicosia","Prague","Copenhagen","Cairo","Helsinki","Paris", "Berlin","Athens","New-Delhi",
         "Tehran","Baghdad","Rome", "Kingston", "Tokyo","Nairobi","Luxembourg","Mexico-City","Amsterdam","Abuja","Oslo",
@@ -22,6 +22,10 @@ window.onload = function () {
     let lives=8;
     console.log(alphabet[0].toString());
     function play(){
+        mylives.innerHTML="Let's Start"
+        var c = document.getElementById("manCanvas");
+        var ctx = c.getContext("2d");
+        ctx.clearRect(0,0,c.width,c.height);
         lives=8;
         var categories=[animal,country,capital];
         var c=Math.floor(Math.random() * categories.length);
@@ -37,6 +41,26 @@ window.onload = function () {
         var tryClick=document.getElementById("tryButton");
         if(tryClick)
             tryClick.addEventListener("click",trying,false);
+        let lettersArray=letters.innerHTML.split("\n");
+        for (var i = 0; i < lettersArray.length; i++) {
+            let letter=lettersArray[i];
+            var letterClick=document.getElementById(letter);
+            if(letterClick)
+                letterClick.addEventListener("click",checking,false);
+            function checking() {
+                let a=0;
+                for (var i = 0; i < wordGlobal.length; i++) {
+                    a=i*40;
+                    if(wordGlobal.charAt(i)===(letter)){
+                        var c = document.getElementById("boxCanvas");
+                        var ctx = c.getContext("2d");
+                        ctx.font = "30px Ubuntu";
+                        ctx.fillStyle = "red";
+                        ctx.fillText("A", (a+15 + (10 * i)), 55);
+                    }
+                }
+            }
+        }
     }
 
     function chooseCat(c) {
@@ -49,8 +73,9 @@ window.onload = function () {
         }
     }
     function check() {
-        for (var i = 0; i < alphabet.length; i++) {
-            let letter=alphabet[i].toString();
+        let lettersArray=letters.innerHTML.split("\n");
+        for (var i = 0; i < lettersArray.length; i++) {
+            let letter=lettersArray[i];
             if(letterClick)
             var letterClick=document.getElementById(letter);
                 letterClick.addEventListener("click",checking,false);
@@ -58,7 +83,7 @@ window.onload = function () {
                 let a=0;
                 for (var i = 0; i < wordGlobal.length; i++) {
                     a=i*40;
-                    if(wordGlobal[i]===(letter)){
+                    if(wordGlobal.charAt(i)===(letter)){
                         var c = document.getElementById("boxCanvas");
                         var ctx = c.getContext("2d");
                         ctx.font = "30px Ubuntu";
@@ -75,7 +100,7 @@ window.onload = function () {
 
         for (var i = 0; i < alphabet.length; i++) {
             letters.id = 'alphabet';
-            list = document.createElement('li');
+            list = document.createElement('button');
             list.id = alphabet[i];
             list.innerHTML = alphabet[i];
             myButtons.appendChild(letters);
@@ -84,107 +109,139 @@ window.onload = function () {
     }
 
     function trying(){
-        if(tryText.value.toLowerCase()===wordGlobal)
-            mylives.innerHTML="Good job"
+        if(lives==0)
+            return
         else {
-            drawArray[8-lives]();
-            lives-=1;
-            if(lives==0){
-                mylives.innerHTML = "Game over";
-            }else {
-                mylives.innerHTML = lives + " lives left";
+            if (tryText.value.toLowerCase() === wordGlobal)
+                mylives.innerHTML = "Good job"
+            else {
+                drawArray[8 - lives]();
+                lives -= 1;
+                if (lives == 0) {
+                    mylives.innerHTML = "Game over";
+                } else {
+                    mylives.innerHTML = lives + " lives left";
+                }
             }
         }
     }
     function makeBoxes(x) {
         var c = document.getElementById("boxCanvas");
         var ctx = c.getContext("2d");
-        ctx.clearRect(0, 0, 2000, 100);
+        ctx.clearRect(0, 0, c.width, c.height);
         let a = 0;
         for (let i = 0; i < x; i++) {
             a =i* 40;
-            ctx.rect((a+10+ (10 * i)), 25, 40, 40);
             ctx.lineWidth = "3";
             ctx.strokeStyle = "blue";
-
-
+            ctx.strokeRect((a+10+ (10 * i)), 25, 40, 40);
         }
-        ctx.stroke();
+
     }
-    var c = document.getElementById("manCanvas");
-    var ctx = c.getContext("2d");
     function drawOne() {
+        var c = document.getElementById("manCanvas");
+        var ctx = c.getContext("2d");
+        ctx.beginPath();
         ctx.moveTo(25, 570);
         ctx.lineWidth = "10";
         ctx.lineTo(125, 570);
+        ctx.closePath();
         ctx.stroke();
     }
 
     function drawTwo() {
+        var c = document.getElementById("manCanvas");
+        var ctx = c.getContext("2d");
+        ctx.beginPath();
         ctx.moveTo(75, 570);
         ctx.lineWidth = "10";
         ctx.lineTo(75, 30);
+        ctx.closePath();
         ctx.stroke();
 
     }
 
     function drawThree() {
+        var c = document.getElementById("manCanvas");
+        var ctx = c.getContext("2d");
+        ctx.beginPath();
         ctx.moveTo(70, 30);
         ctx.lineWidth = "10";
         ctx.lineTo(250, 30);
         ctx.moveTo(75, 150);
         ctx.lineWidth = "10";
         ctx.lineTo(170, 30);
+        ctx.closePath();
         ctx.stroke();
 
     }
 
     function drawFour() {
+        var c = document.getElementById("manCanvas");
+        var ctx = c.getContext("2d");
+        ctx.beginPath();
         ctx.moveTo(245, 30);
         ctx.lineWidth = "10";
         ctx.lineTo(245, 150);
+        ctx.closePath();
         ctx.stroke();
 
     }
 
     function drawFive() {
+        var c = document.getElementById("manCanvas");
+        var ctx = c.getContext("2d");
         ctx.beginPath();
-        ctx.arc(245, 180, 30, 0, 2 * Math.PI);
+        ctx.arc(245, 180, 30, 3*Math.PI/2, -Math.PI/2);
         ctx.lineWidth = "10";
+        ctx.closePath();
         ctx.stroke();
 
     }
 
     function drawSix() {
+        var c = document.getElementById("manCanvas");
+        var ctx = c.getContext("2d");
+        ctx.beginPath();
         ctx.moveTo(245, 210);
         ctx.lineWidth = "10";
         ctx.lineTo(245, 350);
+        ctx.closePath();
         ctx.stroke();
 
     }
 
     function drawSeven() {
+        var c = document.getElementById("manCanvas");
+        var ctx = c.getContext("2d");
+        ctx.beginPath();
         ctx.moveTo(245, 250);
         ctx.lineWidth = "10";
         ctx.lineTo(300, 225);
         ctx.moveTo(245, 250);
         ctx.lineWidth = "10";
         ctx.lineTo(190, 225);
+
+        ctx.closePath();
         ctx.stroke();
 
     }
 
     function drawEight() {
-
+        var c = document.getElementById("manCanvas");
+        var ctx = c.getContext("2d");
+        ctx.beginPath();
         ctx.moveTo(245, 350);
         ctx.lineWidth = "10";
         ctx.lineTo(300, 400);
         ctx.moveTo(245, 350);
         ctx.lineWidth = "10";
         ctx.lineTo(190, 400);
+        ctx.closePath();
         ctx.stroke();
 
     }
-    play();
+
     buttons();
+    play();
 }
